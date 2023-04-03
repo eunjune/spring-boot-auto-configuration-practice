ac7ddc75311e7510c29158e1e772ec4e55dcf5b3
eunjune <iws1605@naver.com>
2023-04-03 17:39:45 +0900

    @Conditional

--- src/main/java/com/example/springbootautoconfigurationpractice/config/MemoryConfig.java
+++ src/main/java/com/example/springbootautoconfigurationpractice/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package com.example.springbootautoconfigurationpractice.config;
 
+import com.example.memory.MemoryCondition;
 import com.example.memory.MemoryController;
 import com.example.memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
 
     @Bean
