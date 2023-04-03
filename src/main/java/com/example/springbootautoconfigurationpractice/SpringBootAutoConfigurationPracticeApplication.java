e7a5259f085c3b1633622c5231ddfb552768db1d
eunjune <iws1605@naver.com>
2023-04-03 16:30:35 +0900

    예제 세팅

---
+++ src/main/java/com/example/springbootautoconfigurationpractice/SpringBootAutoConfigurationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootautoconfigurationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoConfigurationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoConfigurationPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootautoconfigurationpractice/SpringBootAutoConfigurationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootautoconfigurationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoConfigurationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
